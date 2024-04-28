1eb6d7e0a6fab9e68bc171eb99e8b979f27f72ac
AyushSangtani <124431336+AyushSangtani@users.noreply.github.com>
2024-04-28 10:06:53 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; //trate all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+let name = "ayush"
+let age = 21
+let isLogedIn = true
+
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefind 
+//symbol => unique
+
+console.log(typeof "ayush"); //string
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undrfind
